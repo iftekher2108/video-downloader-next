493b6ded4abd813a65f6bcac7734758879992c66
iftekher2108 <parvezkhan21082002@gmail.com>
2025-07-07 18:10:33 +0600

    video downloader next

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Home from "@/component/Home";
+export const metadata = {
+  title: 'video download',
+  description : 'youtube video downloader app'
+}
+
+export default function Index() {
+  return (
+    <Home/>
+  );
+}
